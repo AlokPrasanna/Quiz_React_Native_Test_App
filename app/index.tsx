f004f0d304e853d8bf7e80970d39694dc0d1b5fc
AlokPrasanna <alokprasanna1104@gmail.com>
2024-07-19 14:20:39 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const indx = () => {
+  return (
+    <View>
+      <Text className='text-5xl text-green-600 font-bold text-center mt-5'>Welcome!</Text>
+    </View>
+  )
+}
+
+export default indx
